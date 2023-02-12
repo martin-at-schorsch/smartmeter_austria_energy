ea8c3ef3fa42632d16a20b7b8e5d1998f16dcd93
Christian Neumeier <47736781+NECH2004@users.noreply.github.com>
2023-02-12 10:06:45 +0100

    Read method changed

--- src/smartmeter_austria_energy/smartmeter.py
+++ src/smartmeter_austria_energy/smartmeter.py
@@ -17,121 +17,137 @@ from .obisdata import ObisData
 from .supplier import SUPPLIERS
 
 
-class Smartmeter():
+class Smartmeter:
     """Connects and reads data from the smartmeter."""
-    def __init__(self,
-                 supplier_name: str,
-                 port: str,
-                 key_hex_string : str,
-                 interval : int = 1,
-                 baudrate : int = 2400,
-                 parity: str = serial.PARITY_NONE,
-                 stopbits: str = serial.STOPBITS_ONE,
-                 bytesize: str = serial.EIGHTBITS,
-                 serial_read_chunk_size: int = 100) -> None:
+
+    def __init__(
+        self,
+        supplier_name: str,
+        port: str,
+        key_hex_string: str,
+        interval: int = 1,
+        baudrate: int = 2400,
+        parity: str = serial.PARITY_NONE,
+        stopbits: str = serial.STOPBITS_ONE,
+        bytesize: str = serial.EIGHTBITS,
+        serial_read_chunk_size: int = 100,
+    ) -> None:
         self._supplier_name = supplier_name
         self._port: str = port
         self._key_hex_string = key_hex_string
-        self._baudrate : int = baudrate
+        self._baudrate: int = baudrate
         self._parity: str = parity
         self._stopbits: str = stopbits
         self._bytesize: str = bytesize
         self._interval: int = interval
-        self._serial_read_chunk_size : int = serial_read_chunk_size
-        self._mySerial : serial.Serial = None
-        self._logger = logging.getLogger()
+        self._serial_read_chunk_size: int = serial_read_chunk_size
+        self._mySerial: serial.Serial = None
+        self._logger = logging.getLogger(__name__)
+        self._is_running: bool = False
 
     # read method was mainly taken from https://github.com/tirolerstefan/kaifa
     def read(self) -> ObisData:
-        self.__open_serial()
+        if self._is_running:
+            return None
 
-        supplier = SUPPLIERS.get(self._supplier_name)
-        is_running = self._mySerial.isOpen()
-        self._logger.debug("Start reading from serial.")
+        try:
+            self.__open_serial()
 
-        stream = b''      # filled by serial device
-        frame1 = b''      # parsed telegram1
-        frame2 = b''      # parsed telegram2
+            supplier = SUPPLIERS.get(self._supplier_name)
+            is_running = self._mySerial.isOpen()
+            self._is_running = is_running
 
-        frame1_start_pos = -1          # pos of start bytes of telegram 1 (in stream)
-        frame2_start_pos = -1          # pos of start bytes of telegram 2 (in stream)
+            self._logger.debug("Start reading from serial.")
 
-        sleep_interval = 0.1
-        self._logger.debug("Fetch next frame.")
-        start_time = time.monotonic()
-        max_stop_time = start_time + 5 * self._interval
+            stream = b""  # filled by serial device
+            frame1 = b""  # parsed telegram1
+            frame2 = b""  # parsed telegram2
 
-        # "telegram fetching loop" (as long as we have found two full telegrams)
-        # frame1 = first telegram (68fafa68), frame2 = second telegram (68727268)
-        while is_running:
-            self._logger.debug("Read in chunks.")
-            if (self._mySerial.inWaiting() > 0):
-                # Read in chunks. Each chunk will wait as long as specified by
-                # serial timeout. As the meters we tested send data every 5s the
-                # timeout must be <5. Lower timeouts make us fail quicker.
-                byte_chunk = self._mySerial.read(self._mySerial.inWaiting())
-                stream += byte_chunk
-                frame1_start_pos = stream.find(supplier.frame1_start_bytes)
-                frame2_start_pos = stream.find(supplier.frame2_start_bytes)
+            frame1_start_pos = -1  # pos of start bytes of telegram 1 (in stream)
+            frame2_start_pos = -1  # pos of start bytes of telegram 2 (in stream)
 
-                # fail as early as possible if we find the segment is not complete yet.
-                if((stream.find(supplier.frame1_start_bytes) < 0)
+            sleep_interval = 0.1
+            self._logger.debug("Fetch next frame.")
+            start_time = time.monotonic()
+            max_stop_time = start_time + 5 * self._interval
+
+            # "telegram fetching loop" (as long as we have found two full telegrams)
+            # frame1 = first telegram (68fafa68), frame2 = second telegram (68727268)
+            while is_running:
+                self._logger.debug("Read in chunks.")
+                if self._mySerial.inWaiting() > 0:
+                    # Read in chunks. Each chunk will wait as long as specified by
+                    # serial timeout. As the meters we tested send data every 5s the
+                    # timeout must be <5. Lower timeouts make us fail quicker.
+                    byte_chunk = self._mySerial.read(self._mySerial.inWaiting())
+                    stream += byte_chunk
+                    frame1_start_pos = stream.find(supplier.frame1_start_bytes)
+                    frame2_start_pos = stream.find(supplier.frame2_start_bytes)
+
+                    # fail as early as possible if we find the segment is not complete yet.
+                    if (
+                        (stream.find(supplier.frame1_start_bytes) < 0)
                         or (stream.find(supplier.frame2_start_bytes) <= 0)
                         or (stream[-1:] != supplier.frame2_end_bytes)
-                        or (len(byte_chunk) == self._serial_read_chunk_size)):
+                        or (len(byte_chunk) == self._serial_read_chunk_size)
+                    ):
 
-                    self._logger.debug("Segment is not complete yet.")
-                    actual_time = time.monotonic()
-                    if (actual_time < max_stop_time):
-                        continue
-                    else:
-                        raise SmartmeterTimeoutException()
+                        self._logger.debug("Segment is not complete yet.")
+                        actual_time = time.monotonic()
+                        if actual_time < max_stop_time:
+                            continue
+                        else:
+                            raise SmartmeterTimeoutException()
 
-                if (frame2_start_pos != -1):
-                    # frame2_start_pos could be smaller than frame1_start_pos
-                    if frame2_start_pos < frame1_start_pos:
-                        # start over with the stream from frame1 pos
-                        stream = stream[frame1_start_pos:len(stream)]
-                        continue
+                    if frame2_start_pos != -1:
+                        # frame2_start_pos could be smaller than frame1_start_pos
+                        if frame2_start_pos < frame1_start_pos:
+                            # start over with the stream from frame1 pos
+                            stream = stream[frame1_start_pos : len(stream)]
+                            continue
 
-                    # we have found at least two complete telegrams
-                    regex = binascii.unhexlify('28' + supplier.frame1_start_bytes_hex + '7c' + supplier.frame2_start_bytes_hex + '29')  # re = '(..|..)'
-                    my_list = re.split(regex, stream)
-                    my_list = list(filter(None, my_list))  # remove empty elements
-                    # l after split (here in following example in hex)
-                    # l = ['68fafa68', '53ff00...faecc16', '68727268', '53ff...3d16', '68fafa68', '53ff...d916', '68727268', '53ff.....']
+                        # we have found at least two complete telegrams
+                        regex = binascii.unhexlify("28" + supplier.frame1_start_bytes_hex + "7c" + supplier.frame2_start_bytes_hex + "29")  # re = '(..|..)'
+                        my_list = re.split(regex, stream)
+                        my_list = list(filter(None, my_list))  # remove empty elements
+                        # l after split (here in following example in hex)
+                        # l = ['68fafa68', '53ff00...faecc16', '68727268', '53ff...3d16', '68fafa68', '53ff...d916', '68727268', '53ff.....']
 
-                    # take the first two matching telegrams
-                    for i, el in enumerate(my_list):
-                        if el == supplier.frame1_start_bytes:
-                            frame1 = my_list[i] + my_list[i + 1]
-                            frame2 = my_list[i + 2] + my_list[i + 3]
-                            break
+                        # take the first two matching telegrams
+                        for i, el in enumerate(my_list):
+                            if el == supplier.frame1_start_bytes:
+                                frame1 = my_list[i] + my_list[i + 1]
+                                frame2 = my_list[i + 2] + my_list[i + 3]
+                                break
 
-                    # check for weird result -> exit
-                    if (len(frame1) == 0) or (len(frame2) == 0):
-                        self._logger.debug("Exit because of weird result.")
-                        is_running = False
+                        # check for weird result -> exit
+                        if (len(frame1) == 0) or (len(frame2) == 0):
+                            self._logger.debug("Exit because of weird result.")
+                            is_running = False
 
-                    break
+                        break
 
-            # Optional, but recommended: sleep once per loop to let
-            # other threads on your PC run during this time.
-            time.sleep(sleep_interval)
+                # Optional, but recommended: sleep once per loop to let
+                # other threads on your PC run during this time.
+                time.sleep(sleep_interval)
 
-            actual_time = time.monotonic()
-            if (actual_time < max_stop_time):
-                continue
-            else:
-                raise SmartmeterTimeoutException()
+                actual_time = time.monotonic()
+                if actual_time < max_stop_time:
+                    continue
+                else:
+                    raise SmartmeterTimeoutException()
 
-        self._logger.debug("Next step is decrypting.")
-        dec = Decrypt(supplier, frame1, frame2, self._key_hex_string)
-        dec.parse_all()
+            self._logger.debug("Next step is decrypting.")
+            dec = Decrypt(supplier, frame1, frame2, self._key_hex_string)
+            dec.parse_all()
 
-        obisData = ObisData(dec, supplier.supplied_values)
-        self.__close_serial()
-        return obisData
+            obisData = ObisData(dec, supplier.supplied_values)
+            return obisData
+        except Exception as exception:
+            raise SmartmeterException() from exception
+        finally:
+            self._is_running = False
+            self.__close_serial()
 
     def __open_serial(self):
         try:
@@ -141,7 +157,8 @@ class Smartmeter():
                 parity=self._parity,
                 stopbits=self._stopbits,
                 bytesize=self._bytesize,
-                timeout=self._interval)
+                timeout=self._interval,
+            )
         except SerialTimeoutException as ex:
             self._logger.debug("Timeout happened at closing.")
             raise SmartmeterTimeoutException(f"'{self._port}' has a timeout.") from ex
